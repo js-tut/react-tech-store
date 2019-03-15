eedbb93201341794b72773998c402d6da3cf2bae
js-tut <johnsmilga@gmail.com>
2019-03-14 18:14:40 -0700

    Folder Structure and Page Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default.js";
+
 class App extends Component {
   render() {
     return <h1>hello from tech store</h1>;
